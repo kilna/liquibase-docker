38b0cf8a946f48dce100adbd631effc8131fc5ec
Kilna <kilna@kilna.com>
2017-09-04 21:05:42 -0500

    Initial development

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,15 @@ FROM anapsix/alpine-java
 ARG liquibase_version=3.5.3
 ARG liquibase_download_url=https://github.com/liquibase/liquibase/releases/download/liquibase-parent-${liquibase_version}
 
-SHELL [ "/bin/bash", "-e", "-o", "pipefail", "-c" ]
+ENV LIQUIBASE_DATABASE=${LIQUIBASE_DATABASE:-liquibase}\
+    LIQUIBASE_USERNAME=${LIQUIBASE_USERNAME:-liquibase}\
+    LIQUIBASE_PASSWORD=${LIQUIBASE_PASSWORD:-liquibase}\
+    LIQUIBASE_HOST=${LIQUIBASE_HOST:-db}\
+    LIQUIBASE_CHANGELOG=${LIQUIBASE_CHANGELOG:-changelog.xml}\
+    LIQUIBASE_LOGLEVEL=${LIQUIBASE_LOGLEVEL:-info}
 
-RUN apk --update add curl ca-certificates;\ 
+RUN set -e -o pipefail;\
+    apk --no-cache add curl ca-certificates;\ 
     tarfile=liquibase-${liquibase_version}-bin.tar.gz;\
     mkdir /opt/liquibase;\
     cd /opt/liquibase;\
@@ -16,8 +22,11 @@ RUN apk --update add curl ca-certificates;\
     ln -s /opt/liquibase/liquibase /usr/local/bin/liquibase;\
     mkdir /workspace /opt/jdbc
 
+COPY entrypoint.sh /
 WORKDIR /workspace
-ONBUILD COPY . /workspace
-ONBUILD RUN [[ -e test/run_test.sh ]] && chmod +x test/run_test.sh && mv test/ /opt/liquibase_test/
+
+ONBUILD COPY liquibase.properties /workspace/liquibase.properties
 ONBUILD VOLUME /workspace
+
+ENTRYPOINT [ "/entrypoint.sh" ]
 
---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e -o pipefail
+
+if [[ `ls -A /opt/jdbc/*.jar 2>/dev/null` == '' ]]; then
+  >&2 echo <<'EOF' 
+You appear to be running the liquibase docker image without any drivers loaded
+in /opt/jdbc.  You probably want to running an image with a database driver
+already loaded.  Some images you may can try instead are:
+
+https://hub.docker.com/r/kilna/liquibase-mysql/
+https://hub.docker.com/r/kilna/liquibase-postgres/
+https://hub.docker.com/r/kilna/liquibase-sqlite/
+
+Replacement of ${...} values is disabled for liquibase.properties since it
+needs environment variables which are handled by the database driver
+specific docker images.
+EOF
+echo ">>>"`grep -F '${' liquibase.properties 2>/dev/null`"<<<"
+if [[ `grep -F '${' liquibase.properties 2>/dev/null` != '' ]]; then
+  echo "Replacing variables in liquibase.properties" >&2
+  vars=( URL DATABASE HOST PORT USERNAME PASSWORD CHANGELOG LOGLEVEL )
+  err=0
+  for var in "${vars[@]}"; do
+    lvar="LIQUIBASE_$var"
+    eval "val=\${$lvar:-NULL}"
+    if [[ `grep -F '${'$var'}' liquibase.properties` != '' ]]; then
+      if [[ "$val" == 'NULL' ]]; then
+        echo "\${$var} in liquibase.properties, but env var LIQUIBASE_$var was not set" >&2
+        err=1
+      else
+        echo "Replacing \${$var} with env var LIQUIBASE_$var value '$val'" >&2
+        sed -i 's/\${'${var}'}/'${val}'/g' liquibase.properties
+      fi
+    fi
+  done
+  if [[ "$err" == 0 ]]; then
+    if [[ `grep -F '${' liquibase.properties` != '' ]]; then
+      echo "Unrecognized replacement \${VARIABLE} in liquibase.properties" >&2
+    fi
+  fi
+fi
+
+exec "$@"
+
